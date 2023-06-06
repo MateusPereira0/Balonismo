50c6d98122f60ce17ebacb69ff0cc17b88987052
MateusPereira0 <maat015z@gmail.com>
2023-06-06 08:29:34 -0300

    Balonismo atualizacao

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
      }
 
     public static void main(String[] args) {
-         Pessoa passageiro1 = new Pessoa();
+         // Construir balões
+         Balao b1 = new Balao();
+          String resultado = b1.voar();
+        System.out.println(resultado);
     }
 }
