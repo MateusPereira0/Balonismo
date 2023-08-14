b4819d68fa880de0aa7f93ac7f55ebf984695c9f
Mateus Pereira <126568688+MateusPereira0@users.noreply.github.com>
2023-08-14 16:28:32 -0300

    Update Balao.java

--- src/Balao.java
+++ src/Balao.java
@@ -1,17 +1,85 @@
 public class Balao {
-        // Atributos
-        int id;
-        String registro;
-         String cor;
-          String tipo;
-          int capacidade;
-         double altura;
-        String porte;
-        double peso;
+    //Atributos
+    int id;
+    String registro;
+    String cor;
+    String tipo;
+    int capacidade;
+    double altura;
+    String porte;
+    double peso;
+    //Métodos
+    public String voar(){
+        return "voando";
+    }
 
-       //Métodos
-       public String voar(){
-           return "voando";
-       }
+    public void setId(int id){
 
-        }
+        this.id = id;
+    }
+
+    public int getId(){
+
+        return id;
+    }
+
+    public void setCor(String cor){
+
+        this.cor = cor;
+    }
+
+    public String getCor(){
+
+        return cor;
+    }
+
+    public void setTipo (String tipo){
+
+        this.tipo = tipo;
+    }
+
+    public String getTipo (){
+
+        return tipo;
+    }
+
+    public void setCapacidade(int capacidade){
+
+        this.capacidade = capacidade;
+    }
+
+    public int getCapacidade (){
+
+        return capacidade;
+    }
+
+    public void setAltura(double altura){
+
+        this.altura = altura;
+    }
+
+    public double getAltura (){
+
+        return altura;
+    }
+
+    public void setPorte(String porte){
+
+        this.porte = porte;
+    }
+
+    public String getPorte (){
+
+        return porte;
+    }
+
+    public void peso (double peso){
+
+        this.peso = peso;
+    }
+
+    public double getPeso(){
+
+        return peso;
+    }
+}
